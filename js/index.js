472b9cb997c85ddbbefdfb574b0dba7197810bb2
oscarporras02 <oscarrussi@outlook.com>
2020-11-11 19:11:33 -0500

    show new books in the left side of the page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 const submitButton = document.getElementById('button_form');
+const listBooks = document.getElementById('list-books');
 
 function Book(name, author, genre) {
   this.name = name;
@@ -7,9 +8,20 @@ function Book(name, author, genre) {
   this.genre = genre;
 }
 
+function showBooks() {
+  myLibrary.forEach((book) => {
+    listBooks.innerHTML += `
+      <label>name: ${book.name}</label>
+      <label>author: ${book.author}</label>
+      <label>genre: ${book.genre}</label>
+      `;
+  });
+}
+
 submitButton.onclick = function addBookToLibrary() {
   const name = document.getElementById('book-name').value;
   const author = document.getElementById('book-author').value;
   const genre = document.getElementById('book-genre').value;
   myLibrary.push(new Book(name, author, genre));
+  showBooks();
 };
